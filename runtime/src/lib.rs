bae993715f46a23ab0c78141b8a8ac91afdde6ba
Tatsuki Hashimoto <hashimoto19980924@gmail.com>
2020-11-05 16:06:53 +0900

    fix runtime

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -497,7 +497,7 @@ impl_runtime_apis! {
 			input_data: Vec<u8>,
 		) -> ContractExecResult {
 			let (exec_result, gas_consumed) =
-				CelerContracts::bare_call(origin, dest.into(), value, gas_limit, input_data);
+				Contracts::bare_call(origin, dest.into(), value, gas_limit, input_data);
 			match exec_result {
 				Ok(v) => ContractExecResult::Success {
 					flags: v.flags.bits(),
@@ -511,27 +511,27 @@ impl_runtime_apis! {
 		fn resolve(
 			virt_addr: Hash,
 		) -> celer_contracts_primitives::ResolveResult<AccountId> {
-			CelerContracts::resolve(virt_addr)
+			Contracts::resolve(virt_addr)
 		}
 
 		fn generate_offchain_address(
 			code_hash: Hash,
 			nonce: u128
 		) -> Hash {
-			CelerContracts::generate_offchain_address(code_hash, nonce)
+			Contracts::generate_offchain_address(code_hash, nonce)
 		}
 
 		fn get_storage(
 			address: AccountId,
 			key: [u8; 32],
 		) -> celer_contracts_primitives::GetStorageResult {
-			CelerContracts::get_storage(address, key)
+			Contracts::get_storage(address, key)
 		}
 
 		fn rent_projection(
 			address: AccountId,
 		) -> celer_contracts_primitives::RentProjectionResult<BlockNumber> {
-			CelerContracts::rent_projection(address)
+			Contracts::rent_projection(address)
 		}
 	}
 
